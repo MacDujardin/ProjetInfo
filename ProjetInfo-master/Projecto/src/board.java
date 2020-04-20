7616c298ef057ca26bc1bba9282643dc42a41349
Tirette-hub <nathan.amorison@gmailcom>
2020-04-20 16:38:19 +0200

    ajout de la classe Board

--- ProjetInfo-master/Projecto/src/board.java
+++ ProjetInfo-master/Projecto/src/board.java
@@ -1,15 +1,34 @@
+public class Board {
+	public int size ;
+	public String [] [] board;
+	
+	public  Board (int size) {	
+      this.size = size; //9 cases de pions et 8 cases de murs
+      board = new String [size] [size] ;
+      for ( int row = 0; row<board.length; row ++ ) {
+    	  board[row] [0]= "pawn1win" ;
+      }
+      for ( int row = 0; row<board.length ; row ++) { 
+    	  board [row] [size] = "pawn2win";
+      }
 
-public class board {
-	private final int size = 17; //9 cases de pions et 8 cases de murs
- private Tile [][] tiles;
- 
- public void tileMaker() {
-	 tiles = new Tile[size][size];
-	 for (int i=0 ;  i< tiles.length ; i++) {
-		 for (int j=0 ; j < tiles.length ; j++) {
-			 tiles [i][j]= new Tile( i , j);
-		 }
-	 }
- }
- 
+}
+	public void setValue (Vector pos , String value) {
+	//Assigne une valeur (value) à la position (pos) souhaitée
+	  board [pos.y][pos.x] = value ; 
+	} 
+	public String getValue (Vector pos , Vector vect ) {
+		return board [pos.y+vect.y][pos.x+vect.x];
+	}
+	public String getValue (Vector pos) {
+		return board [pos.y][pos.x];
+	}
+	public Vector getPosByValue (String value) {
+		for (int y=0 ; y<size ; y ++ ) {
+			for (int x=0; x<size ;x++) {
+				if (board [y][x] == value )
+					return Vector(x,y);	
+			}
+		}
+	}
 }

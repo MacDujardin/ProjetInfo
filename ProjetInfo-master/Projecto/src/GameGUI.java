d871b2e987e63fd89a7f27af121d2df41d1682ad
Tirette-hub <nathan.amorison@gmailcom>
2020-04-30 17:13:03 +0200

    [FRONTEND] Nouvelle classe GUI avec mouse listener

--- ProjetInfo-master/Projecto/src/GameGUI.java
+++ ProjetInfo-master/Projecto/src/GameGUI.java
@@ -1,36 +1,84 @@
 import javax.swing.*;
 import java.util.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 public class GameGUI extends JFrame {
 	
 	int spacing =5;
 	
-    public GameGUI() {
-    	this.setTitle("Projo");
-    	this.setSize(1286 ,829);
-    	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	this.setVisible(true);
-    	this.setResizable(false);
+	int mx = -100;
+	int my = -100;
+	
+      public GameGUI() {
+    	  this.setTitle("Projo");
+    	  this.setSize(1286 ,829);
+    	  this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	  this.setVisible(true);
+    	  this.setResizable(false);
     	  
-        Board board = new Board();
-    	this.setContentPane(board);
-    }
+    
+    	  
+    	  Move move = new Move();
+    	  this.addMouseMotionListener(move);
+    	  
+    	  Click click = new Click();
+    	  this.addMouseListener(click);
+    	  
+      }
       
-    public class Board extends JPanel {
-        //IGOR
-    	public void paintComponent(Graphics x) {
-    	    x.setColor(Color.DARK_GRAY);
-    	    x.fillRect(0 , 0 , 1280 , 800 );
-    	    x.setColor(Color.gray);
-    		for (int i=0 ; i < 9 ; i++) {
-    		    for (int j= 0 ; j < 9; j++) {
-    			    x.fillRect(spacing+i*80+280, spacing+j*80+40, 80-2*spacing, 80-2*spacing);
-    		    }
-    		}
-    		x.setColor(Color.red);
-    		x.fillRect(240, 0, 10 , 800);
-    		x.fillRect(1030, 0, 10,800); 
-    	}
-    }
+      
+      
+      public class Move implements MouseMotionListener {
+
+		@Override
+		public void mouseDragged(MouseEvent e) {
+		
+			
+		}
+
+		@Override
+		public void mouseMoved(MouseEvent e) {
+		mx=e.getX();
+		my=e.getY();
+			System.out.println(mx +" et " +my);
+		}
+    	  
+      }
+      public class Click implements MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+    	  
+      }
 }
+

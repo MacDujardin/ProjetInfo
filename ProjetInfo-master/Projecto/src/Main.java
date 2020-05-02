f4a58e9bd46c836d89f8006382c7e2a78521fdf8
Tirette-hub <nathan.amorison@gmailcom>
2020-05-02 14:20:03 +0200

    [FRONTEND] début du visuel du plateau

--- ProjetInfo-master/Projecto/src/Main.java
+++ ProjetInfo-master/Projecto/src/Main.java
@@ -1,19 +1,107 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class Main implements Runnable {
+public class Main extends Application  {
 
-	GameGUI gui = new GameGUI();
-	
+	Stage quoridor;
+	Scene home, mode1v1 , modeIAhome , modeIAgood ,modeIAdumb ;
+	public int i;
 	
 	public static void main(String[] args) {
-		new Thread(new Main()).start();
-		
+	launch(args);
 	}
 
 	@Override
-	public void run() {
-		while(true) {
-			gui.repaint();
-		}
+	public void start(Stage primaryStage) throws Exception {
+		quoridor = primaryStage;
+		
+		Label label1 = new Label("Bienvenue sur Quoridor , veuillez choisir un mode de jeu");
+		Button button1v1 = new Button("Joueur contre Joueur");
+		Button buttonhomeIA = new Button("Joueur contre l'IA");
+		button1v1.setOnAction(e -> quoridor.setScene(mode1v1));
+		buttonhomeIA.setOnAction(e -> quoridor.setScene(modeIAhome));
+		
+		VBox layout1 = new VBox(20);
+		layout1.getChildren().addAll(label1 , button1v1 , buttonhomeIA);
+		home = new Scene (layout1 , 500 , 500);
+		
+		Button buttonhome = new Button("retour au menu");
+		buttonhome.setOnAction(e -> quoridor.setScene(home));
+		GridPane grid1v1 = new GridPane();
+        grid1v1.setGridLinesVisible(true);
+        final int numCols = 17 ;
+        final int numRows = 17 ;
+        for ( i = 0; i < numCols; i++) {
+        	if (i%2 == 0) {
+            ColumnConstraints colConst = new ColumnConstraints();
+            colConst.setPercentWidth(100.0 / numCols);
+            grid1v1.getColumnConstraints().add(colConst);
+        	} 
+        	else {
+        		ColumnConstraints colConst = new ColumnConstraints();
+                colConst.setPercentWidth(20.0 / numCols);
+                grid1v1.getColumnConstraints().add(colConst);
+        	}
+        }
+        for ( i = 0; i < numRows; i++) {
+        	if( i%2 == 0 ) {
+            RowConstraints rowConst = new RowConstraints();
+            rowConst.setPercentHeight(100.0 / numRows);
+            grid1v1.getRowConstraints().add(rowConst);         
+        }
+        	else {
+        		 RowConstraints rowConst = new RowConstraints();
+                 rowConst.setPercentHeight(20.0 / numRows);
+                 grid1v1.getRowConstraints().add(rowConst);  	
+        	}
+        }
+        grid1v1.add(buttonhome, 1 , 0 ,1 ,1);
+		BorderPane layout2 = new BorderPane();
+		layout2.setCenter(grid1v1 );
+		layout2.setTop(buttonhome);
+		mode1v1 = new Scene (layout2 , 800 , 800);
+		
+		Label label3 = new Label("choisir une IA");
+		Button buttonIAgood = new Button("Smart");
+		Button buttonIAdumb = new Button("Dullard");
+		buttonIAgood.setOnAction(e -> quoridor.setScene(modeIAgood));
+		buttonIAdumb.setOnAction(e-> quoridor.setScene(modeIAdumb));
+		
+		VBox layout3 = new VBox(20);
+		layout3.getChildren().addAll(label3 ,buttonIAgood , buttonIAdumb);
+		modeIAhome = new Scene (layout3 , 500 , 500);
+		
+		Label label4 = new Label("Il est con");
+		Button returnhome1 = new Button("Im out");
+		returnhome1.setOnAction(e -> quoridor.setScene(home));
+		
+		VBox layout4 = new VBox();
+		layout4.getChildren().addAll(label4 , returnhome1);
+		modeIAdumb = new Scene(layout4 , 500 ,500);
+		
+		Label label5 = new Label("Malin");
+		Button returnhome2 = new Button("Im out");
+		returnhome2.setOnAction(e -> quoridor.setScene(home));
+		
+		VBox layout5 = new VBox();
+		layout5.getChildren().addAll(label5 , returnhome2);
+		modeIAgood = new Scene (layout5 ,500 ,500);
+		
+		quoridor.setScene(home);
+		quoridor.setTitle("Quoridor");
+		quoridor.show();
+		
 	}
+
+	
 
 }
